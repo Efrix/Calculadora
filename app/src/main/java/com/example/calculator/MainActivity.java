e2ee8b04a62de0f723bd3508b1668f7b0ab7aea3
Efrix <zukis5826@gmail.com>
2024-12-14 18:19:05 -0700

    Cambios en el proyecto 1

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.calculator;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        new Timer().schedule(new TimerTask() {
+            public void run() {
+                MainActivity.this.startActivity(new Intent(MainActivity.this, Calculator.class));
+                MainActivity.this.finish();
+            }
+        }, 1200);
     }
 }
